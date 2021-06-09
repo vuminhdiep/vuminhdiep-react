9c5301f59987c381240cf107ee477a9585282154
Vu Minh Diep <vuminhdiep.hycmd@gmail.com>
2021-06-10 01:52:38 +0700

    Update Portfolio.js

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -33,7 +33,7 @@ class Portfolio extends Component {
         <Fade left duration={1000} distance="40px">
           <div className="row">
             <div className="twelve columns collapsed">
-              <h1>Check Out Some of My Projects.</h1>
+              <h1>Check Out Some of My Projects (To be updated).</h1>
 
               <div
                 id="portfolio-wrapper"
